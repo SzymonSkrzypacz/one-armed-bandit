a3d9b65eed9b7e6f76968a82e8f8dd754e9301cf
Szymon Skrzypacz <skrzypak2@gmail.com>
2021-05-26 17:50:03 +0200

    created walletClass and methods

---
+++ walletClass.js
@@ -0,0 +1,26 @@
+class walletClass {
+  constructor(money) {
+    let _monay = monay;
+
+    this.getValueOfWallet = () => _monay;
+
+    this.checkCanPlay = (value) => {
+      if (_monay >= value) return true;
+      return false;
+    };
+
+    this.changeWallet = (value, type = "+") => {
+      if (typeof value === "number" && !isNaN(value)) {
+        if (type === "+") {
+          return (_monay += value);
+        } else if (type === "-") {
+          return (_monay -= value);
+        }
+      } else {
+        throw new Error("That is not a number!");
+      }
+    };
+  }
+}
+
+const wallet = new walletClass(100);
