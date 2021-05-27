2f01c259c5d79fdf3e38cbb158bc3bd636a2f97a
Szymon Skrzypacz <skrzypak2@gmail.com>
2021-05-27 18:21:46 +0200

    added DrawClass and some fix

---
+++ js/DrawClass.js
@@ -0,0 +1,19 @@
+class DrawClass {
+  constructor() {
+    this.options = ["gold", "maroon", "silver"];
+    let _result = this.drawResult();
+    this.getDrawResult = () => _result;
+  }
+
+  drawResult() {
+    let colors = [];
+    for (let i = 0; i < this.options.length; i++) {
+        const index = Math.floor(Math.random()*this.options.length);
+        const color = this.options[index];
+        colors.push(color);
+    }
+    return colors;
+  }
+}
+
+const draw = new DrawClass();
